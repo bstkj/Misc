40ad2419ccb93c98027a159aedc60756d0cae5d2
Benjamin Teo <bteo@lunchbox.stat.wisc.edu>
2020-08-16 23:10:16 -0500

    Add comments to rock_paper_scissors.jl

--- julia/games/rock_paper_scissors.jl
+++ julia/games/rock_paper_scissors.jl
@@ -1,6 +1,15 @@
+#=
+# A small exercise to experiment with Julia's type system, as well as its
+# multiple-dispatch feature.
+# Decided to throw in a little randomness and get acquainted with the 
+# Distributions package.
+=#
+
 using Distributions
 
 abstract type choice end
+# `rock`, `paper`, `scissors` are singleton types. There can only be one 
+# instance of such types.
 struct rock <: choice end
 struct paper <: choice end
 struct scissors <: choice end
@@ -8,15 +17,20 @@ struct scissors <: choice end
 r = rock(); p = paper(); s = scissors()
 
 struct player
+    # `stats` is a representation of the pmf over the 3 possible choices 
     stats::Vector{Float64, 1}
 end
 
+# Some of Julia's syntactical sugar for making objects callable by adding methods
+# to its type
 function (p::player)()
+    # Randomly pick a choice, considering the player's propensities
     [r, p, s][rand(Categorical(p.stats), 1)]
 end
 
 result(::rock, ::scissors) = 1
 result(::paper, ::rock) = 1
 result(::scissors, ::paper) = 1
+# Dispatch can lead to meaningfully-succinct code
 result(::T, ::T) where T<:choice = 0
 result(::choice, ::choice) = -1
